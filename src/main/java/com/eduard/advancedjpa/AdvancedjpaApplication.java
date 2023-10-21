51f07dafa089795cd2354be47422b7dc0eb0518e
Eduard Epurica <epurica.eduard@gmail.com>
2023-10-21 18:26:57 +0300

    Initial Commit

---
+++ src/main/java/com/eduard/advancedjpa/AdvancedjpaApplication.java
@@ -0,0 +1,13 @@
+package com.eduard.advancedjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduard/advancedjpa/AdvancedjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduard.advancedjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
