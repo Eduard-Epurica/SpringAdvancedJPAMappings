2bbb4600a0e3e2d38611cf80c3d1af8c59541c23
Eduard Epurica <epurica.eduard@gmail.com>
2023-10-23 11:18:25 +0300

    Initial Commit

--- src/main/java/com/eduard/advancedjpa/AdvancedjpaApplication.java
+++ src/main/java/com/eduard/advancedjpa/AdvancedjpaApplication.java
@@ -1,13 +1,28 @@
 package com.eduard.advancedjpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdvancedjpaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AdvancedjpaApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello world");
+		};
 	}
 
 }
+
+
+
